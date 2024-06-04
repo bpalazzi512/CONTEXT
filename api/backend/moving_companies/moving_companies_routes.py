ee6dc7e0df8b6dca2857087fceae204a8b242596
bradylii <braydrt2d2@gmail.com>
2024-06-04 23:20:06 +0200

    update SQL and api

--- api/backend/moving_companies/moving_companies_routes.py
+++ api/backend/moving_companies/moving_companies_routes.py
@@ -8,7 +8,20 @@ from backend.db_connection import db
 moving_company = Blueprint('moving_company', __name__)
 
 # Get a list of all moving companies for a given route 
-
+@moving_company.route('/moving_company', methods=['GET'])
+def get_routes():
+    current_app.logger.info('moving_company.py: GET /moving_comapny')
+    cursor = db.get_db().cursor()
+    cursor.execute('select m.id, name, bio, from countries')
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
 # Get a list of all the countryIDs a moving company can move to
 
 # Get a list of all the stateIDs a moving company can move from
@@ -21,4 +34,4 @@ moving_company = Blueprint('moving_company', __name__)
 
 # Post (add) a country to a moving company's list of movable locations
 
-# Get a list of all users (name, email, phone) that contacts a moving company
+# Get a list of all users (name, email, phone) that contacts a moving company by date
