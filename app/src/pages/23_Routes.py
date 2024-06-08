9151a86b634d72b1d7e3a344220848c4e2839d8f
gavinsan33 <gavinsan33@gmail.com>
2024-06-08 17:16:26 +0200

    fixed cols

--- app/src/pages/23_Routes.py
+++ app/src/pages/23_Routes.py
@@ -7,13 +7,12 @@ import streamlit as st
 import requests
 from modules.nav import SideBarLinks
 
-st.set_page_config(layout = 'wide')
+st.set_page_config(layout='wide')
 
 # Show appropriate sidebar links for the role of the currently logged in user
 SideBarLinks()
 
 # Sample data for customers
-
 data = {}
 moverID = st.session_state['id']
 try:
@@ -23,15 +22,6 @@ except:
 
 df = pd.DataFrame(data)
 
-col1, col2= st.columns(2)
-
-
-
-moveLoad_txt = df["moveLoad"][0]
-st.write(moveLoad_txt)
-
-edit_buttons = {}
-del_buttons = {}
 # Function to display the routes
 def display_routes(df):
     header_cols = st.columns([3, 4, 4, 2, 2, 2])
@@ -39,36 +29,22 @@ def display_routes(df):
     header_cols[1].write("**Destination**")
     header_cols[2].write("**Load**")
     header_cols[3].write("**Rate**")
+    header_cols[4].write("**Edit**")
+    header_cols[5].write("**Delete**")
+
+    edit_buttons = {}
+    del_buttons = {}
 
     for index, row in df.iterrows():
         cols = st.columns([3, 4, 4, 2, 2, 2])
-
-        moveLoad_txt = df["moveLoad"][index]
-        cost_txt = df['cost'][index]
-        # with col0:
-        #     st.write(cols[0].write(row["stateName"]))
-        # with col1:
-        #     st.write(cols[1].write(row["name"]))
-        with col1:
-            col2, col3 = st.columns(2)
-            st.write(row["stateName"])
-            st.write(row["name"])
-        with col2:
-            cols[2] = st.selectbox('', ['Full Household', 'Part Household', 
-                                     'Personal Effects Only', 'Excess Baggage', 'Vehicle Only'], 
-                                     key =f"moveL_{index}")
-            #cols[2] = st.text_input('', moveLoad_txt,  placeholder='moveLoad',
-                                #key =f"moveL_{index}")
-        with col3:
-            cols[3] = st.text_input('', cost_txt, placeholder='cost', 
-                               key =f"cost_{index}")
-        #cols[2].write(row["moveLoad"])
-        #cols[3].write('$' + str(row["cost"]))
-        edit_buttons[index] = cols[4].button('Update', key = f"edit_{index}")
-        
-        del_buttons[index] = cols[5].button('Delete', key = f"del_{index}")
+        cols[0].write(row["stateName"])
+        cols[1].write(row["name"])
+        cols[2].selectbox('', ['Full Household', 'Part Household', 'Personal Effects Only', 'Excess Baggage', 'Vehicle Only'], index=0, key=f"moveL_{index}")
+        cols[3].text_input('', row['cost'], key=f"cost_{index}")
+        edit_buttons[index] = cols[4].button('Update', key=f"edit_{index}")
+        del_buttons[index] = cols[5].button('Delete', key=f"del_{index}")
         route_id = df['id'][index]
-    #for index, delete in del_buttons.items():
+
         if del_buttons[index]:
             response = requests.delete(f'http://api:4000/r/del_routes/{route_id}')
             if response.status_code == 200:
@@ -77,26 +53,12 @@ def display_routes(df):
                 st.error(f"Failed delete route {route_id}")
 
         if edit_buttons[index]:
-            data = {"moveLoad" : cols[2], # moveLoad input
-                    "cost" : cols[3], # cost input
-                    "id" : str(route_id)} # routeID
+            data = {"moveLoad": cols[2].value, "cost": cols[3].value, "id": str(route_id)}
             response = requests.put(f'http://api:4000/r/routes_edit', json=data)
             if response.status_code == 200:
-                st.success(f"edited {route_id}")
+                st.success(f"Edited {route_id}")
             else:
                 st.error(f"Failed to edit {route_id}")
-                
-# if st.button("Save Bio"):
-#     data = {"countryID" : countryID, "bio" : bio}
-#     requests.put('http://api:4000/c/country/bio', json=data)
-#     st.success("Section updated successfully!")
-
-#     fromStateID int NOT NULL,
-#     toCountryID int NOT NULL,
-#     moverID int NOT NULL,
-#     moveLoad ENUM('Full Household', 'Part Household', 'Personal Effects Only', 'Excess Baggage', 'Vehicle Only') NOT NULL,
-#     cost int,
-#     id int NOT NULL,
 
 # Page Title
 st.title(f"Routes for {st.session_state['name']}")
@@ -104,10 +66,8 @@ display_routes(df)
 
 userID = st.session_state['id']
 
-
 def add_route():
     with st.form(key='new_route_form'):
-        # make origin selectbox with all 50 states listed alphabetically  
         origin = st.selectbox('Origin', ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 
                                          'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 
                                          'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 
@@ -118,7 +78,6 @@ def add_route():
                                          'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 
                                          'Wisconsin', 'Wyoming'])
         
-        # all EU countries alphabetically
         destination = st.selectbox('Destination', ['Austria', 'Belgium', 'Bulgaria', 'Croatia', 'Cyprus', 'Czech Republic',
                                                    'Denmark', 'Estonia', 'Finland', 'France', 'Germany', 'Greece', 'Hungary',
                                                    'Ireland', 'Italy', 'Latvia', 'Lithuania', 'Luxembourg', 'Malta', 'Netherlands',
@@ -130,29 +89,25 @@ def add_route():
 
         if submit_button:
             new_data = {
-                "MoverID" : st.session_state['id'],
-                "Origin": [origin],
-                "Destination": [destination],
-                "Load": [load],
-                "Rate": [rate]
+                "MoverID": st.session_state['id'],
+                "Origin": origin,
+                "Destination": destination,
+                "Load": load,
+                "Rate": rate
             }
-            st.write("hello")
             try: 
                 response = requests.post("http://api:4000/r/add_route", json=new_data)
             except:
                 st.write("Did not add!")
             if response.status_code == 201 or response.status_code == 200:
-                st.success("Post submitted successfully!")
+                st.success("Route added successfully!")
+                new_df = pd.DataFrame([new_data])
+                global df
+                df = pd.concat([df, new_df], ignore_index=True)
+                st.success("New route added successfully!")
             else:
-                st.error("Failed to submit post. Please try again.")
-            new_df = pd.DataFrame(new_data)
-            global df
-            df = pd.concat([df, new_df], ignore_index=True)
-            st.success("New route added successfully!")
-
+                st.error("Failed to add route. Please try again.")
 
 # Button to add a new route
 if st.button('Add New Route'):
     add_route()
-
-
