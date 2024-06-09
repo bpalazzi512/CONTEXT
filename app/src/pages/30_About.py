3a937d13ebde09839ddf7a53c414c18caa792eaf
gavinsan33 <gavinsan33@gmail.com>
2024-06-09 22:23:34 +0200

    delted junk

--- app/src/pages/30_About.py
+++ app/src/pages/30_About.py
@@ -8,15 +8,17 @@ st.write("# About this App")
 
 st.markdown (
     """
+
+    Welcome to Explore Your Future Home, a comprehensive platform designed to help retirees 
+    and expatriates find their ideal country to move to. This project aims to provide valuable 
+    insights into various countries, considering multiple factors such as happiness index, crime safety, 
+    leisure activities, cost of living, and more. Whether you’re planning your retirement abroad or simply 
+    exploring new possibilities, our platform equips you with the necessary information to make an informed decision.
+    
     This app is being built as an exemplar for Northeastern University's 
     Summer 2024 Dialogue of Civilization Program titled *Data and 
     Software in International Government and Politics*.  The program is bein
     led by Dr. Mark Fontenot and Dr. Eric Gerber from the Khoury College of
     Computer Sciences.  
-
-    The goal of this demo is to provide information on the tech stack 
-    being used as well as demo some of the features of the various platforms. 
-
-    Stay tuned for more information and features to come!
     """
         )
