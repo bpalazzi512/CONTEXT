c961c005e74a54b371deb45fafcd4817f3104ff9
gavinsan33 <gavinsan33@gmail.com>
2024-06-09 17:57:09 +0200

    push

--- app/src/Home.py
+++ app/src/Home.py
@@ -1,5 +1,5 @@
 import logging
-logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 import streamlit as st
