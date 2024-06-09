19e37013384b46bb6d47b084fc9e64943311631c
gavinsan33 <gavinsan33@gmail.com>
2024-06-09 11:56:27 +0200

    crime stuff

--- database/04_context.sql
+++ database/04_context.sql
@@ -33,6 +33,7 @@ CREATE TABLE IF NOT EXISTS countries (
     id INT UNIQUE NOT NULL,
     name VARCHAR(50) UNIQUE NOT NULL,
     happinessIndex DOUBLE,
+    crime_safety int,
     railwayLength INT,
     unemploymentRate DECIMAL(5, 1),
     avg_temp DECIMAL(5, 2),
