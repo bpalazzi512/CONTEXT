172168d1e672a75c1f3107d817a0c367731c28ee
gavinsan33 <gavinsan33@gmail.com>
2024-06-09 23:35:21 +0200

    about

--- app/src/pages/30_About.py
+++ app/src/pages/30_About.py
@@ -14,8 +14,6 @@ st.markdown (
     leisure activities, cost of living, and more. Whether you’re planning your retirement abroad or simply 
     exploring new possibilities, our platform equips you with the necessary information to make an informed decision.
     
-    ![logo](assets/logo.png)
-
     This app is being built as an exemplar for Northeastern University's 
     Summer 2024 Dialogue of Civilization Program titled *Data and 
     Software in International Government and Politics*.  The program is bein
