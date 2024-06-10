0ceeb4b87f3e87e4ebda336b388bd7d08c5cac86
gavinsan33 <gavinsan33@gmail.com>
2024-06-10 10:04:01 +0200

    sidebar size change

--- app/src/modules/nav.py
+++ app/src/modules/nav.py
@@ -43,7 +43,7 @@ def SideBarLinks(show_home=False):
     """    
 
     # add a logo to the sidebar always
-    st.sidebar.image("assets/logo.png", width = 250)
+    st.sidebar.image("assets/logo.png", width = 200)
 
     # If there is no logged in user, redirect to the Home (Landing) page
     if 'authenticated' not in st.session_state:
